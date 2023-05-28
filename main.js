c222e1aa2a1ebb653b1e30e8160308d684e4acbb
Kim Joohee <syap1170@gmail.com>
2023-05-28 23:13:35 +0900

    moviebox add

--- main.js
+++ main.js
@@ -1,28 +1,35 @@
-document.getElementById("movieCard").innerHTML =
-  `<div class="card mb-3" style="max-width: 540px;">
-    <div class="row g-0">
-      <div class="col-md-4">
-        <img src="url 주소" class="img-fluid rounded-start" alt="...">
-      </div>
-      <div class="col-md-8">
-        <div class="card-body">
-          <h2 class="card-title"></h2>
-          <h4 class="card-text">영화 소개</h4>
-          <h4 class="card-text"><small class="text-muted">영화 별점</small></h4>
-        </div>
-      </div>
-    </div>
-  </div>`;
-
 const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNDMwZWRmMWNlZGQzZGQ4ODUwMWYwNGQ4YzQwNmEzYiIsInN1YiI6IjY0NzA4ZTY5NzcwNzAwMDBmYzAyNTM1YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.A1ljtMKfFX7Gq9Xrj5oyk_KiEE1-JIcVp3LvQrHCsHE'
-  }
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNDMwZWRmMWNlZGQzZGQ4ODUwMWYwNGQ4YzQwNmEzYiIsInN1YiI6IjY0NzA4ZTY5NzcwNzAwMDBmYzAyNTM1YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.A1ljtMKfFX7Gq9Xrj5oyk_KiEE1-JIcVp3LvQrHCsHE'
+    }
 };
 
 fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
+    .then(response => response.json())
+    .then((data) => {
+        // 영화 목록
+        let movieList = data['results']
+
+        movieList.forEach((a) => {
+            let poster_path = a['poster_path']
+            let title = a['title']
+            let overview = a['overview']
+            let vote_average = a['vote_average']
+            let id = a['id']
+            const movieBox = document.getElementById("movieBox");
+            // console.dir(movieBox.innerHTML);
+
+            document.getElementById("movieBox").innerHTML =
+                `<div class="movieBox">
+                    <img src="https://image.tmdb.org/t/p/w500/${poster_path}" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">${title}</h5>
+                        <p class="card-text">${overview}</p>
+                        <p>${vote_average}</p>
+                    </div>
+                </div>`
+        })
+    })
+    .catch(err => console.error(err));
