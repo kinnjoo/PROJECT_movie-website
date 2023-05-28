4c5f010732fbe6095328f8c7f0b86d89229266a3
Kim Joohee <syap1170@gmail.com>
2023-05-28 13:07:47 +0900

    movie box

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+document.getElementById("movieCard").innerHTML =
+  `<div class="card mb-3" style="max-width: 540px;">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src="url 주소" class="img-fluid rounded-start" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h2 class="card-title"></h2>
+          <h4 class="card-text">영화 소개</h4>
+          <h4 class="card-text"><small class="text-muted">영화 별점</small></h4>
+        </div>
+      </div>
+    </div>
+  </div>`;
+
 const options = {
   method: 'GET',
   headers: {
