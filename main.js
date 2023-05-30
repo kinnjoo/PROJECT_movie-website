09360fcc099e890b60c6680f9fc14b5739d00528
Kim Joohee <syap1170@gmail.com>
2023-05-30 17:58:27 +0900

    search function add

--- main.js
+++ main.js
@@ -9,30 +9,51 @@ const options = {
 fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
     .then(response => response.json())
     .then(data => {
-        // 영화 목록 확인
-        // console.log(data['results']);
+        // 영화 목록
         let movieList = data['results'];
 
-        movieList.forEach(Movie => {
-            let poster_path = Movie['poster_path'];
-            let title = Movie['title'];
-            let overview = Movie['overview'];
-            let vote_average = Movie['vote_average'];
-            let id = Movie['id'];
+        const list = document.getElementById('movieBox');
 
-            const movieElement = document.createElement("movieBox");
-            movieElement.innerHTML = `
-                <div onclick="movieId(${id})">
-                    <img src="https://image.tmdb.org/t/p/w500/${poster_path}">
-                    <p>${title}</p>
-                    <p>${overview}</p>
-                    <p>${vote_average}</p>
-                </div>
-            `;
+        // 영화 목록을 표시
+        function showList(val = '') {
+            // movieBox 내용 지우고 forEach문으로 movieList 데이터입력
+            list.innerHTML = "";
 
-            movieBox.appendChild(movieElement);
-        });
+            movieList.forEach(movie => {
+                let poster_path = movie['poster_path'];
+                let title = movie['title'];
+                let overview = movie['overview'];
+                let vote_average = movie['vote_average'];
+                let id = movie['id'];
+
+                // 검색하면(검색 값이 있을경우) list에 추가 -> 검색 결과 출력
+                if (title.toLowerCase().includes(val.toLowerCase())) {
+                    const movieElement = document.createElement("div");
+                    movieElement.className = "movieCard";
+                    movieElement.innerHTML = `
+                        <img src="https://image.tmdb.org/t/p/w300/${poster_path}" onclick="movieId(${id})">
+                        <p>${title}</p>
+                        <p>${overview}</p>
+                        <p>${vote_average}</p>
+                    `;
+                    list.appendChild(movieElement);
+                }
+            });
+        }
+
+        showList();
+
+        const searchInput = document.getElementById("searchInput");
+        const searchBtn = document.getElementById("searchBtn");
+
+        // 검색 버튼
+        searchBtn.addEventListener("click", (e) => {
+            e.preventDefault();
+            const val = searchInput.value;
+            showList(val);
+        })
     })
     .catch(err => console.error(err));
 
+// 영화이미지 클릭시 id값 alert 창으로 띄우기
 const movieId = id => alert(`영화 id : ${id}`);
